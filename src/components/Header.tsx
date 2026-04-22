fb8f6a59ce82199600f886069aeae9b59bd2065b
RodrigoBoehme <robi.boehme@gmail.com>
2026-04-22 20:04:02 -0300

    Added everything ele

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+// Componente simples de cabeçalho
+export default function Header() {
+  return (
+    <header style={{ textAlign: "center", marginBottom: "20px",borderRadius:"135px",background:"linear-gradient(136deg, red,blue) "}}>
+      <h1>🌌 NASA - Foto do Dia</h1>
+      <p>Escolha uma data e veja a imagem do universo!</p>
+    </header>
+  );
+}
